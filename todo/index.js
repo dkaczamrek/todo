9d95182b97b2fb27519f3b330efa80f923b71585
Dawid Kaczmarek <dkaczamrek@gmail.com>
2019-04-10 22:29:23 +0200

    dodawanie zadań w obrębie jednej funkcji + ukrywanie 'Lista zadań jest pusta' do wyje***ia

--- todo/index.js
+++ todo/index.js
@@ -1,11 +1,24 @@
-var newTaskContent = document.getElementById('new-task-content').value;
+var newTaskContent;
 var buttonAddTask = document.getElementById('add-task');
 
-console.log(newTaskContent);
 
+//do wyjabania
+var tastListEmpyInfo = document.getElementById('tast-list-empty');
+
+var newTask;
+var insertTask;
 
 buttonAddTask.addEventListener('click', captureTaskContent);
 
 function captureTaskContent(){
-    console.log(newTaskContent);
+    newTaskContent = document.getElementById('new-task-content').value;
+    if (newTaskContent) {
+        newTask = document.createElement('li');
+        newTask.className = 'task';
+        newTask.innerHTML = newTaskContent;
+        document.getElementById('task-list').appendChild(newTask);
+        newTaskContent = document.getElementById('new-task-content').value = "";
+        //do wyjabania
+        tastListEmpyInfo.style.display = 'none';
+    }
 }
