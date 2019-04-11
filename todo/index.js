e498d328c7f93bb2be1c04db5a871d68127bd839
Dawid Kaczmarek <dkaczamrek@gmail.com>
2019-04-11 22:10:09 +0200

    poprawiony komunikat 'Lista zadań jest pusta', przekreślenie pierwszego elementu

--- todo/index.js
+++ todo/index.js
@@ -1,12 +1,10 @@
 var newTaskContent;
 var buttonAddTask = document.getElementById('add-task');
 
-
-//do wyjabania
+var taskStriker;
 var tastListEmpyInfo = document.getElementById('tast-list-empty');
 
 var newTask;
-var insertTask;
 
 buttonAddTask.addEventListener('click', captureTaskContent);
 
@@ -18,7 +16,22 @@ function captureTaskContent(){
         newTask.innerHTML = newTaskContent;
         document.getElementById('task-list').appendChild(newTask);
         newTaskContent = document.getElementById('new-task-content').value = "";
-        //do wyjabania
-        tastListEmpyInfo.style.display = 'none';
     }
+}
+
+buttonAddTask.addEventListener('click', taskCounter);
+
+function taskCounter(){
+    var taskCount = document.getElementsByTagName('li').length;
+    if (taskCount > 0) {
+        
+        tastListEmpyInfo.style.display = 'none';
+        taskStriker = document.getElementsByTagName('li')[0];
+        console.log(taskStriker);
+        taskStriker.addEventListener('click', strikeThrough);
+    }
+}
+
+function strikeThrough(){
+    document.getElementsByTagName('li')[0].style.textDecoration = "line-through";
 }
