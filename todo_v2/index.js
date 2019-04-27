24dec86d1a3b5965701b619a0dbe476ce7a031f5
Dawid Kaczmarek <dkaczamrek@gmail.com>
2019-04-27 20:27:02 +0200

    sortowanie AZ - ZA (dodać index żeby wrócić do sortowania domyślnego)

--- todo_v2/index.js
+++ todo_v2/index.js
@@ -1,11 +1,14 @@
 var addButton = document.getElementById('add-task'),
-list = document.getElementById('task-list'),
-info = document.getElementById('tast-list-empty'),
-sort = document.getElementById('sort');
+    list = document.getElementById('task-list'),
+    info = document.getElementById('tast-list-empty'),
+    sort = document.getElementById('sort'),
+    sortType;
+
 
 sort.setAttribute('disabled', 'disabled');
 
-var taskList = [];
+var taskList = loadList() || [];
+renderList();
 
 function captureContent() {
     var taskContent = document.getElementById('new-task-content').value;
@@ -19,12 +22,55 @@ function captureContent() {
         text: taskContent,
         crossed: false
     });
+
     document.getElementById('new-task-content').value = '';
     document.getElementById('new-task-content').classList.remove('required');
+
     renderList();
 }
 
 function renderList() {
+    saveList(taskList);
+
+
+    switch (sortType) {
+        case 'sortDefault':
+            console.log('domyślnie');
+            break;
+        case 'sortAZ':
+            taskList.sort(function (a, b) {
+                if (a.text > b.text) {
+                    return 1;
+                }
+
+                if (a.text < b.text) {
+                    return -1;
+                }
+
+                return 0;
+            });
+            break;
+        case 'sortZA':
+            taskList.sort(function (a, b) {
+                if (a.text < b.text) {
+                    return 1;
+                }
+
+                if (a.text > b.text) {
+                    return -1;
+                }
+
+                return 0;
+            });
+            break;
+        case 'sortManual':
+            console.log('ręcznie');
+            break;
+    }
+
+
+    
+
     if (taskList.length) {
         info.style.display = 'none';
         sort.removeAttribute('disabled');
@@ -55,7 +101,6 @@ function renderList() {
         deleteButton.innerHTML = 'x';
         createTask.appendChild(deleteButton);
     }
-
 }
 
 addButton.addEventListener('click', captureContent);
@@ -64,22 +109,42 @@ document.addEventListener('click', function (e){
 
     if (e.target.matches('li.task')) {
         i = e.target.getAttribute('data-index');
-        console.log(i);
 
-        if (taskList[i].crossed === true) {
+        if (taskList[i].crossed) {
             taskList[i].crossed = false;
         } else {
             taskList[i].crossed = true;
         }
+    
+        renderList();
     };
 
     if (e.target.matches('button.delete')) {
         i = e.target.parentNode.getAttribute('data-index');
         taskList.splice(i, 1);
 
+        renderList();
     }
 
-    renderList();
+    if (e.target.matches('#sort')) {
+        sortType = sort.value;
+
+        renderList();
+    }
 });
 
-console.log();
+
+
+
+
+function loadList() {
+    try {
+        return JSON.parse(localStorage.getItem('list'));
+    } catch (e) {
+        return [];
+    }
+}
+
+function saveList(list) {
+    localStorage.setItem('list', JSON.stringify(list));
+}
